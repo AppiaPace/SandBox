81f8388848b12aff1958c678dfe0a2047aea6c68
sviollet <samuel.viollet@gmail.com>
2014-01-28 19:59:02 +0100

    test
    test

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SandBox
+//
+//  Created by Samuel VIOLLET on 28/01/2014.
+//  Copyright (c) 2014 Samuel VIOLLET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APPAppDelegate class]));
+    }
+}
